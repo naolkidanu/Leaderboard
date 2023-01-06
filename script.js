5392de9b4ac0af2493d79257fb2857e1a81c2836
naolkidanu <sabelkidanu18@gmail.com>
2023-01-06 01:55:20 -0800

    Add list

---
+++ script.js
@@ -0,0 +1,21 @@
+import './styles.css';
+
+const scores = [
+  {
+    name: 'Naol',
+    score: 50,
+  },
+  {
+    name: 'Kidanu',
+    score: 40,
+  },
+  {
+    name: 'Merdasa',
+    score: 60,
+  },
+];
+
+const scoresEl = document.querySelector('.scores');
+scores.forEach(({ name, score }) => {
+  scoresEl.innerHTML += `<li class="score">${name} : ${score}</li>`;
+});
